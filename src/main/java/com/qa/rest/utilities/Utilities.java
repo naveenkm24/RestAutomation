590630fe0fddaae4ee235c4275baf1131eb439a7
DESKTOP-CPBMLHS\Naveen <npj@shutterfly.com>
2019-10-29 04:44:00 -0500

    Adding utilities File

---
+++ src/main/java/com/qa/rest/utilities/Utilities.java
@@ -0,0 +1,23 @@
+package com.qa.rest.utilities;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Utilities {
+	
+	public static String empName() {
+		String generateString = RandomStringUtils.randomAlphabetic(2);
+		return ("John" + generateString);
+	}
+	
+	public static String empSal() {
+		String generateString = RandomStringUtils.randomNumeric(7);
+		return (generateString);
+	}
+	
+	public static String empAge() {
+		String generateString = RandomStringUtils.randomNumeric(2);
+		return (generateString);
+		
+	}
+
+}
